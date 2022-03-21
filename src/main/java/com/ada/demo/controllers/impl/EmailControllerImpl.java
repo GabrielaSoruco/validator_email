c4fbc568beab672db05a37b5198b704f86fb13a0
GabrielaSoruco <soruco.gabi@gmail.com>
2022-03-21 01:12:29 -0300

    config swagger

--- src/main/java/com/ada/demo/controllers/impl/EmailControllerImpl.java
+++ src/main/java/com/ada/demo/controllers/impl/EmailControllerImpl.java
@@ -3,6 +3,8 @@ package com.ada.demo.controllers.impl;
 import com.ada.demo.controllers.EmailController;
 import com.ada.demo.entities.Email;
 import com.ada.demo.service.EmailService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Api(tags = "Usuarios")
 @RestController
+@RequestMapping("/")
 public class EmailControllerImpl implements EmailController {
     private int contador = 0;
     private boolean emailValid = false;
@@ -20,6 +24,10 @@ public class EmailControllerImpl implements EmailController {
     @Autowired
     EmailService emailService;
 
+    @ApiOperation(
+            value = "Devuelve una cadena de texto",
+            notes = "Verifica que la api responde, si retorna <b>Funciona</b> se pueden usar los servicios de la api. De otro modo, revisar"
+    )
     @Override
     @GetMapping("/test")
     public ResponseEntity<String> test() {
@@ -29,6 +37,9 @@ public class EmailControllerImpl implements EmailController {
         return new ResponseEntity<String>(msg, header, HttpStatus.OK);
     }
 
+    @ApiOperation(
+            value = "Lista de emails válidos"
+    )
     @Override
     @GetMapping(value = "/emailsValid")
     public ResponseEntity<List<Email>> getValidEmails() {
@@ -38,6 +49,9 @@ public class EmailControllerImpl implements EmailController {
         return ResponseEntity.status(HttpStatus.OK).headers(header).body(validEmails);
     }
 
+    @ApiOperation(
+            value = "Lista de emails no válidos"
+    )
     @Override
     @GetMapping(value = "/emailsNotValid")
     public ResponseEntity<List<Email>> getInvalidEmails() {
@@ -45,6 +59,9 @@ public class EmailControllerImpl implements EmailController {
         return ResponseEntity.status(HttpStatus.OK).body(emailsNotValid);
     }
 
+    @ApiOperation(
+            value = "Email según id"
+    )
     @Override
     @GetMapping(value = "/emails/{id}")
     public ResponseEntity<Optional<Email>> getEmailById(@PathVariable Long id) {
@@ -52,6 +69,10 @@ public class EmailControllerImpl implements EmailController {
         return ResponseEntity.ok(emailById);
     }
 
+    @ApiOperation(
+            value = "Devuelve un texto",
+            notes = "Si el email es válido retorna <b>es válido</b>, si no lo es retorna <b>el email no es válido</b>"
+    )
     @Override
     @GetMapping("/emails/validate/{email}")
     public ResponseEntity<String> validarEmail(@PathVariable("email") String email) {
@@ -67,7 +88,10 @@ public class EmailControllerImpl implements EmailController {
         }
     }
 
-
+    @ApiOperation(
+            value = "Devuelve el número de intentos",
+            notes = "Desde que se envía un email para verificar si es válido, la api cuenta la cantidad de intentos hasta que el email sea válido"
+    )
     @GetMapping("/emails/validate/intentos")
     public ResponseEntity<String> getIntentos(){
         int auxContador = contador;
@@ -78,6 +102,10 @@ public class EmailControllerImpl implements EmailController {
         return ResponseEntity.ok(msg);
     }
 
+    @ApiOperation(
+            value = "Guarda email verificado",
+            notes = "Si se comprobó que el email es válido, entonces se tiene la opción de crear y guardar un usuario en la base de datos, enviando solo el email que se verificó"
+    )
     @Override
     @PostMapping(value = "/emails/validate/add")
     public ResponseEntity<Email> addEmail(@RequestBody Email email) {
--- src/main/java/com/ada/demo/swagger/SwaggerConfig.java
+++ src/main/java/com/ada/demo/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.ada.demo.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -10,7 +11,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -26,25 +26,14 @@ public class SwaggerConfig {
                 .apiInfo(getApiInfo());
     }
 
+
     private ApiInfo getApiInfo(){
-        return new ApiInfo(
-                "Order Service API",
-                "Order Service API Description",
-                "1.0",
-                "http://codmind.com/terms",
-                new Contact("Codmind", "https://codmind.com", "apis@codmind.com"),
-                "LICENSE",
-                "LICENSE URL",
-                Collections.emptyList()
-        );
+        return new ApiInfoBuilder()
+                .title("Validación de emails")
+                .version("1.0")
+                .description("Api para verificar si un email es válido o no.")
+                .contact(new Contact("Gabriela Soruco", "https://github.com/GabrielaSoruco", "soruco.gabi@gmail.com"))
+                .license("license")
+                .build();
     }
 }
-/*
-*
-* "SWAGGER DEMO DOCUMENTATION", //Name
-                "WWW.EJEMPLOSWAGGER.COM", //Description
-                "VERSION 1.0", //Version
-                "TERMS OF SERVICE URL", //Terms of service url
-                 new Contact("Gabriela Soruco", "HTTPS://WWW.JAVADESDE0.COM", "SORUCO.GABI@GMAIL.COM"),
-                "LICENCIA", //License
-                "HTTP://WWW.GOOGLE.COM" //Terms of license url */
